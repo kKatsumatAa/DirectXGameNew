1c2d4331da4a229f7144b6a025bfd7edbe8a30ea
LC1A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-07-30 13:11:02 +0900

    03_08_ex1

--- EnemyBullet.cpp
+++ EnemyBullet.cpp
@@ -8,10 +8,26 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position, const Vector
 	model_ = model;
 
 	//テクスチャ読み込み
-	textureHandle_ = TextureManager::Load("scope.png");
+	textureHandle_ = TextureManager::Load("red.png");
 
 	worldTransform.Initialize();
 	worldTransform.translation_ = position;
+
+	//zに伸びた形状
+	worldTransform.scale_.x = 0.5f;
+	worldTransform.scale_.y = 0.5f;
+	worldTransform.scale_.z = 3.0f;
+
+	//yじくまわり
+	worldTransform.rotation_.y = std::atan2(velocity_.x, velocity_.z);
+	//y軸周りに-θ回す回転行列
+	Matrix4 m4;
+	SetRotationMatrix(m4, -worldTransform.rotation_.y, 'y');
+	//velocityに回転行列を掛け算してvelocityZを求める（資料参照）
+	Vector3 v = velocity_;
+	Vector3xMatrix4(v, m4, false);//ここで上下の角度を反転
+	//x軸周り角度
+	worldTransform.rotation_.x = std::atan2(-v.y, v.z);
 }
 
 void EnemyBullet::Update()
